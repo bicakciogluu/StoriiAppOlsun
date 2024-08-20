5c824f484bb2bb39a6a0785e276486e4fab3ed28
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2024-08-20 15:28:45 +0300

    Initial Commit

---
+++ StoriiAppOlsun/StoriiAppOlsunApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriiAppOlsunApp.swift
+//  StoriiAppOlsun
+//
+//  Created by Ömer Bıçakcıoğlu on BE 20.08.2567.
+//
+
+import SwiftUI
+
+@main
+struct StoriiAppOlsunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
